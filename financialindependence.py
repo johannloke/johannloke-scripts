6730321663ac3985cd3c9798e64e9922632b7aba
johannloke <calamityloke@gmail.com>
2020-01-01 21:17:21 +0800

    Update financialindependence.py
    Included Input Validation with While Loop and Change input to Float.

--- financialindependence.py
+++ financialindependence.py
@@ -1,19 +1,39 @@
-#Prompts user to enter the amount spend monthly then multiplies value by 12 to get annual spend
-print("Please enter the amount you spend a month on average (Inclusive of Insurance, Rental, etc): ")
-monthly_spend = input("Enter here: ")
-annual_spend = float(monthly_spend) * 12
+#Prompts user to enter the amount spend monthly
+spending_is_number = False
+rate_is_number = False
+save_invest_is_number = False
+
+while spending_is_number == False:
+    try:
+        print("Please enter the amount you spend a month on average (Inclusive of Insurance, Rental, etc): ")
+        monthly_spend = float(input("Enter here: "))
+        spending_is_number = True
+    except ValueError as err:
+        print(err, "Please enter numbers only (For example: 1000).")
 
 #Prompts user to enter the rate of return for their investments/savings
-print("Please enter the rate of return on average (1 percent return : 0.01%): ")
-rate_of_return = input("Enter here: ")
+while rate_is_number == False:
+    try:
+        print("Please enter the rate of return on average (1 percent return : 0.01%): ")
+        rate_of_return = float(input("Enter here: "))
+        rate_is_number = True
+    except ValueError as err:
+        print(err, "Please enter decimal only (For example: 0.02).")
 
 #Prompts user to enter the amount that they save/invest monthly then multiplies value by 12 to get annual savings/investments
-print("How much do you save/invest a month on average (Inclusive of Savings Accounts, Investments, Bonds, etc)")
-amount_not_spent = input("Enter here: ")
-annual_amountnotspent = float(amount_not_spent) * 12
+while save_invest_is_number == False:
+    try:
+        print("How much do you save/invest a month on average (Inclusive of Savings Accounts, Investments, Bonds, etc)")
+        amount_not_spent = float(input("Enter here: "))
+        save_invest_is_number = True
+    except ValueError:
+        print("Please enter numbers only (For example: 3000).")
+
+annual_spend = monthly_spend * 12
+annual_amountnotspent = amount_not_spent * 12
 
 #Calculates the amout of money to reach Financial Independence by taking the annual spend and dividing by the rate of returns
-amount_to_reach_FI = float(annual_spend) / float(rate_of_return)
+amount_to_reach_FI = annual_spend / rate_of_return
 
 #Calculates the years to reach Financial Independence by taking the amount to reach Financial Independence diving by Annual Spend
 years_to_reach = amount_to_reach_FI / annual_amountnotspent
